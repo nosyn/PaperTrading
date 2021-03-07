3e6e95cc5ea612a11e6f02562a919ba2181d2403
Son Nguyen <son.nguyen@plusonerobotics.com>
2021-03-06 22:38:15 -0600

    fix small things

--- web-server/src/configs.js
+++ web-server/src/configs.js
@@ -1,8 +1,11 @@
-import keys from "../../.env/keys";
+const keys = require("../../.env/keys");
 
 const serverConfigs = Object.freeze({
   PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.NODE_ENV === "production" ? process.env.MONGO_URI : keys.MONGO_URI;
+  MONGO_URI:
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URI
+      : keys.MONGO_URI,
 });
 
 module.exports = { serverConfigs };
